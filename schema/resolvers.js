b1f8d29cf8a137d1e83f8e9d02092714b55dd467
Sahil Arora <sahilaroraji2002@gmail.com>
2023-11-27 13:04:02 +0530

    working with resolvers

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,12 +1,38 @@
-const { userList} = require("../FakeData")
+const { UserList, MovieList } = require("../FakeData");
 
+const _ = require("lodash");
 
 const resolvers = {
-   Query:{
-      users(){
-         return userList;
-      }
-   }
-}
+  Query: {
+    users: () => {
+      console.log(UserList);
+      return UserList;
+    },
+    user: (parent, args) => {
+      const id = args.id;
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
+    },
 
-module.exports = {resolvers}
+    movies: () => {
+      return MovieList;
+    },
+
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name });
+      return movie;
+    },
+  },
+  User: {
+   favoriateMovies: () => {
+      return _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      );
+    },
+  },
+};
+
+module.exports = { resolvers };
